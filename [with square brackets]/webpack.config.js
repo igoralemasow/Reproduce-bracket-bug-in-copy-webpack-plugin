1fd0284692c896e3fc1a8de647b6130f7321f03b
Flawid DSouza <flawid@live.in>
2018-02-23 12:07:46 +0530

    the bug

---
+++ [with square brackets]/webpack.config.js
@@ -0,0 +1,13 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+    entry: './entry.js',
+    output: {
+        filename: 'output.js'
+    },
+    plugins: [
+        new CopyWebpackPlugin([
+            {from:'from', to:'to'} 
+        ]), 
+    ]
+}
